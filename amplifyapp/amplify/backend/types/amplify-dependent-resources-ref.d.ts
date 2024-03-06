112dbc892d6d3d2141577eb82ee0882f279ab6da
Subhash Dike <subhasdd@amazon.com>
2024-03-05 22:00:41 -0500

    added auth

---
+++ amplifyapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "rdscacheamplifyapp935f1597": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
