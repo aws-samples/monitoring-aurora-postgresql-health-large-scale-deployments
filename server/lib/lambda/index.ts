3150c8855c44003c250637d9c869989375f402a9
Subhash Dike <subhasdd@amazon.com>
2024-04-04 18:38:18 -0400

    Updated lambda

--- server/lib/lambda/index.ts
+++ server/lib/lambda/index.ts
@@ -4,7 +4,7 @@ import { RDS } from '@aws-sdk/client-rds';
 import { EventBridgeHandler, EventBridgeEvent, Context, Callback } from 'aws-lambda';
 
 const getCloudWatchMetric = async (cloudwatch: CloudWatch, db_instance_id: string, startTime: Date, endTime: Date) => {
-    console.log(`Getting CloudWatch metric for ${db_instance_id}`);
+    //console.log(`Getting CloudWatch metric for ${db_instance_id}`);
     const data = await cloudwatch.getMetricStatistics({
         Namespace: 'AWS/RDS',
         MetricName: 'BufferCacheHitRatio',
@@ -43,35 +43,32 @@ async function listAuroraPostgreSQLInstanceIds(): Promise<string[]> {
 
 const iterateLogs = async () => {
     const instanceIds = await listAuroraPostgreSQLInstanceIds();
-
-    for (let day = 1; day <= 10; day++) {
-        console.log(`Calculating for ${day}`);
-        //Calculate number of days backwards from now until day
-        const startTime = new Date();
-        startTime.setDate(startTime.getDate() - day);
-        const endTime = new Date();
-        endTime.setDate(endTime.getDate() - day + 1);
-        for (let hour = 0; hour < 24; hour++) {
-            for (const instanceId of instanceIds) {
-                console.log(`Calculating for ${instanceId} for ${hour}`);
-                const cloudwatch = new CloudWatch({ region: 'us-east-1' });
-                const dataPoints = await getCloudWatchMetric(cloudwatch, instanceId, startTime, endTime);
-                console.log(dataPoints);
-                //Iterate dataPoints
-                for (const dataPoint of dataPoints) {
-                    //if datapoint average <= 99.9 create a report_item object with instanceId, datapoint average and starttime
-                    if (dataPoint.Average && dataPoint.Average <= 99.9) {
-                        //format datapoint.average as a string with 2 decimal places
-                        const average = dataPoint.Average.toFixed(2);
-                        //format startTime into Year-Month-Date Hour and Timezone format
-                        const startTimeFormatted = startTime.toLocaleString('en-US', { timeZoneName: 'short' })
-                        //insert ReportItem in dynamodb table named BufferCacheHitRatioMetrics
-                        insertIntoDynamoDb(instanceId, average, startTimeFormatted);
-                    }
+    const startTime = new Date();
+    const endTime = new Date();
+    for (let hour = 0; hour < 24; hour++) {
+        //set startTime to hour and endtime to startime plus one hour
+        startTime.setHours(hour, 0, 0, 0);
+        endTime.setHours(hour + 1, 0, 0, 0);
+        for (const instanceId of instanceIds) {
+            console.log(`Calculating for ${instanceId} for ${startTime} and ${endTime}`);
+            const cloudwatch = new CloudWatch({ region: 'us-east-1' });
+            const dataPoints = await getCloudWatchMetric(cloudwatch, instanceId, startTime, endTime);
+            console.log(dataPoints);
+            //Iterate dataPoints
+            for (const dataPoint of dataPoints) {
+                //if datapoint average <= 99.9 create a report_item object with instanceId, datapoint average and starttime
+                if (dataPoint.Average && dataPoint.Average <= 99.9) {
+                    //format datapoint.average as a string with 2 decimal places
+                    const average = dataPoint.Average.toFixed(2);
+                    //format startTime into Year-Month-Date Hour and Timezone format
+                    const startTimeFormatted = startTime.toLocaleString('en-US', { timeZoneName: 'short' })
+                    //insert ReportItem in dynamodb table named BufferCacheHitRatioMetrics
+                    insertIntoDynamoDb(instanceId, average, startTimeFormatted);
                 }
             }
         }
     }
+
 }
 
 const insertIntoDynamoDb = async (instanceId: string, average: string, startTimeFormatted: string) => {
